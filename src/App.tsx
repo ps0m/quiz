34d6b4cb91bfd7354c28cda0ecfef7f42050d7a9
Sergey Pobudey <s.pobudey@pulkovo-service.ru>
2025-05-08 20:50:39 +0300

    remove characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Quiz from './components/Quiz';
 
 function App() {
   return (
-    <div className="app">
-      <div className="characters">
-        <div className="mary-poppins">
-          <div className="mary-head"></div>
-          <div className="mary-hat"></div>
-          <div className="mary-body"></div>
-          <div className="mary-skirt"></div>
-          <div className="mary-umbrella"></div>
-        </div>
-        <div className="little-prince">
-          <div className="prince-head"></div>
-          <div className="prince-hair"></div>
-          <div className="prince-body"></div>
-          <div className="prince-cape"></div>
-          <div className="prince-legs"></div>
-          <div className="prince-planet"></div>
-        </div>
-      </div>
+      <div className="app">
       <div className="phone-frame">
         <div className="dots-pattern"></div>
         <Quiz />
